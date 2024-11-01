db0e19377fae5b61c82c8fd26ad92400d5f0e516
EC2 Default User <ec2-user@ip-172-31-34-85.ap-northeast-1.compute.internal>
2024-11-01 09:55:53 +0000

    editted notification table to add foreign key and checked all tables colomn and db migrated

--- db/migrate/20241101094009_create_notifications.rb
+++ db/migrate/20241101094009_create_notifications.rb
@@ -8,5 +8,11 @@ class CreateNotifications < ActiveRecord::Migration[6.1]
       t.datetime :read_at  # 読まれていない通知も存在させたいため"null: false"は外す
       t.timestamps
     end
+    
+    # 外部キーの追加
+    add_foreign_key :notifications, :users, column: :user_id
+    # notifiable_id と notifiable_type にインデックスを追加
+    add_index :notifications, [:notifiable_type, :notifiable_id]
+    #複数のカラムに対する「複合インデックス」を設定したいため、手動でインデックスを設定
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_11_01_083430) do
+ActiveRecord::Schema.define(version: 2024_11_01_094729) do
+
+  create_table "admin_notifications", force: :cascade do |t|
+    t.bigint "admin_id", null: false
+    t.text "message", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "admins", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -23,6 +30,72 @@ ActiveRecord::Schema.define(version: 2024_11_01_083430) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "commentable_id", null: false
+    t.string "commentable_type", null: false
+    t.text "content", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "likable_type", null: false
+    t.integer "likable_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["likable_type", "likable_id"], name: "index_likes_on_likable"
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.text "message", null: false
+    t.integer "notifiable_id", null: false
+    t.string "notifiable_type", null: false
+    t.datetime "read_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["notifiable_type", "notifiable_id"], name: "index_notifications_on_notifiable_type_and_notifiable_id"
+  end
+
+  create_table "ownerships", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "ticket_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.text "text_content", null: false
+    t.bigint "ticket_id"
+    t.bigint "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tickets", force: :cascade do |t|
+    t.string "name", null: false
+    t.bigint "issuer_id", null: false
+    t.bigint "recipient_id", null: false
+    t.text "description", null: false
+    t.date "expiration_date", null: false
+    t.integer "quantity", null: false
+    t.integer "available_quantity", null: false
+    t.integer "status", default: 0, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -40,4 +113,7 @@ ActiveRecord::Schema.define(version: 2024_11_01_083430) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "users"
+  add_foreign_key "likes", "users"
+  add_foreign_key "notifications", "users"
 end

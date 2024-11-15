765fd0e1e2bb0b6123d95525cbc0ce6c96ec3816
EC2 Default User <ec2-user@ip-172-31-34-85.ap-northeast-1.compute.internal>
2024-11-15 11:26:47 +0000

    editted routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  
   devise_for :admins, controllers: {
   registrations: "admin/registrations",
   sessions: "admin/sessions"
@@ -7,5 +8,71 @@ Rails.application.routes.draw do
   registrations: "public/registrations",
   sessions: 'public/sessions'
   }
+  # Public側
+  scope module: :public do
+    # Homes
+    root to: 'homes#top' # トップページ
+    get 'about', to: 'homes#about'
+
+    # Users
+    get 'mypage', to: 'users#mypage'
+    resources :users, only: [:index, :show, :edit, :update, :destroy] do
+      member do
+        get 'followers'
+        get 'followings'
+      end
+    end
+
+    # Posts
+    resources :posts do
+      collection do
+        get 'likes', to: 'posts#like_index'
+      end
+      resources :comments, only: [:create, :destroy] # ポストに対するコメント
+    end
+
+    # Tickets
+    get 'tickets/users/:id', to: 'tickets#my_tickets', as: 'user_tickets' # 自分の所持しているチケット一覧
+    resources :tickets do
+      member do
+        get 'transfer'
+      end
+      resources :comments, only: [:create, :destroy] # チケットに対するコメント
+    end
+    
+    
+    # Notifications
+    resources :notifications, only: [:index, :show, :destroy]
+
+    # Likes
+    resources :likes, only: [:create, :destroy] do
+      collection do
+        get 'users/:id/posts', to: 'likes#index_posts', as: 'user_post_likes'
+        get 'users/:id/tickets', to: 'likes#index_tickets', as: 'user_ticket_likes'
+      end
+    end
+
+    # Ownerships
+    resources :ownerships, only: [:create, :destroy]
+
+    # Relationships (follows/unfollows)
+    resources :relationships, only: [:create, :destroy]
+  end
+
+  # Adminネームスペース
+  namespace :admin do
+    # Admin Users
+    resources :users, only: [:index, :show, :edit, :update, :destroy] do
+      member do
+        get 'followers'
+        get 'followings'
+      end
+    end
+
+    # Admin notifications
+    resources :admin_notifications, only: [:new, :create, :index]
+  end
+  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

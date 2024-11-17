db55d49aac72509099618b009116e4824ef77b16
EC2 Default User <ec2-user@ip-172-31-34-85.ap-northeast-1.compute.internal>
2024-11-17 08:49:21 +0000

    editted posts controller to add basic functions

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,2 +1,61 @@
 class Public::PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  
+  
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+
+    if @post.save
+      redirect_to post_path(@post), notice: '投稿が作成されました。'
+    else
+      flash.now[:alert] = '投稿の作成に失敗しました。'
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def index
+    @posts = Post.all.order(created_at: :desc).page(params[:page]).per(10)
+  end
+
+  def edit
+    redirect_to posts_path, alert: '編集権限がありません。' unless @post.user == current_user
+  end
+
+  def update
+    if @post.user == current_user && @post.update(post_params)
+      redirect_to @post, notice: '投稿が更新されました。'
+    else
+      flash.now[:alert] = '投稿の更新に失敗しました。'
+      render :edit
+    end
+  end
+
+  def destroy
+    if @post.user == current_user
+      @post.destroy
+      redirect_to posts_path, notice: '投稿が削除されました。'
+    else
+      redirect_to posts_path, alert: '削除権限がありません。'
+    end
+  end
+  
+  private
+
+  def post_params
+    params.require(:post).permit(:text_content, :image, :ticket_id)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+  
 end

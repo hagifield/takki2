2adc5204e881d3b5ab66d1bfdf74f2cf3ffbe23a
EC2 Default User <ec2-user@ip-172-31-34-85.ap-northeast-1.compute.internal>
2024-11-29 10:43:22 +0000

    editted notifications and relationship controllers to create notification for following

--- app/controllers/public/notifications_controller.rb
+++ app/controllers/public/notifications_controller.rb
@@ -3,14 +3,13 @@ class Public::NotificationsController < ApplicationController
 
   # 通知一覧
   def index
-    @notifications = current_user.notifications.order(created_at: :desc).page(params[:page])
+    @notifications = current_user.notifications.order(created_at: :desc).page(params[:page]).per(10)
   end
 
   # 通知を既読にする
   def show
     @notification = current_user.notifications.find(params[:id])
     @notification.mark_as_read
-    redirect_to polymorphic_path(@notification.notifiable)
   end
 
   # 通知を削除
--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -4,7 +4,9 @@ class Public::RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:followed_id])
     current_user.follow(@user)
-    # 通知を作成するロジックは後で追加
+    # 通知を作成
+    create_notification(@user)
+
     redirect_to request.referer || users_path, notice: "#{@user.name} をフォローしました"
   end
 
@@ -16,5 +18,14 @@ class Public::RelationshipsController < ApplicationController
   end
 
   private
+  
+  # 通知を作成するメソッド
+  def create_notification(followed_user)
+    followed_user.notifications.create(
+      notifiable: current_user, # フォローしてきたユーザー
+      action_type: 'follow',
+      message: "#{current_user.name} さんがあなたをフォローしました"
+    )
+  end
 
 end
